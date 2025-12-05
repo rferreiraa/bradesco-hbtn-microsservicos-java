3910d59e2a4bc040c5bc831734edc8475277177d
Rodrigo <rferreiraa02@gmail.com>
2025-12-05 12:14:56 -0300

    0. Construindo uma API REST com Spring Boot com validação e tratamento de exceções - USER API

---
+++ 0x06/api-rest-userController/src/main/java/com/example/demo/exception/ExceptionService.java
@@ -0,0 +1,37 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionService {
+
+    @ExceptionHandler
+    public ResponseEntity<UserErrorResponse> handleIdException(UserIdException err) {
+        UserErrorResponse uer = new UserErrorResponse();
+        uer.setStatus(HttpStatus.BAD_REQUEST.value());
+        uer.setMessage("You have entered ID " + err.getMessage() + " invalid.");
+
+        return new ResponseEntity<>(uer, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<UserErrorResponse> handleUserNameException(UserNameException err) {
+        UserErrorResponse uer = new UserErrorResponse();
+        uer.setStatus(HttpStatus.BAD_REQUEST.value());
+        uer.setMessage("You have entered USERNAME " + err.getMessage() + " invalid.");
+
+        return new ResponseEntity<>(uer, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<UserErrorResponse> handleCPFException(CPFException err) {
+        UserErrorResponse uer = new UserErrorResponse();
+        uer.setStatus(HttpStatus.BAD_REQUEST.value());
+        uer.setMessage("You have entered CPF " + err.getMessage() + " invalid.");
+
+        return new ResponseEntity<>(uer, HttpStatus.BAD_REQUEST);
+    }
+}
